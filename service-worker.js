46eb4b06aff47634f31066ac960c770cb27763d9
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-01-13 06:38:55 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ function makeFile(name, contenttype, content){
   eval(`
   if (url.pathname === '` + name + `') {
     event.respondWith(
-      fetch('text.txt')
+      fetch('.')
         .then(response => {
             return new Response(atob('` + content + `'), {
               status: 200,
